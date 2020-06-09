672ef237d79526694c03f8d20cc85bd4000114fe
Matt Lee <crazoter@gmail.com>
2020-06-09 12:57:51 +0800

    Fix symbolic links creation

--- scripts/setup/create_symbolic_links_alice.sh
+++ scripts/setup/create_symbolic_links_alice.sh
@@ -2,11 +2,15 @@
 
 # Create symbolic links for ease of access (See https://kb.iu.edu/d/abbe)
 # Ensure that the working directory is the setup directory.
+# Pre-requisites: run mkdir_alice.sh first.
 
 # Symbolic link container
 if [ ! -d ../../build/ALICE ]
 then
   cd ../../build
+
+  DIR="$PWD"
+  echo $DIR
 
   mkdir ALICE
   mkdir ALICE/DIR
@@ -14,37 +18,43 @@ then
 
   # Alice
   # Directories
-  ln -s ./alice/data/timestamp_events            ALICE/DIR/TIMESTAMPS
-  ln -s ./alice/data/type1-time_and_click        ALICE/DIR/T1
-  ln -s ./alice/data/type2-time_and_basis        ALICE/DIR/T2
-  ln -s ./alice/data/type3-rawkey                ALICE/DIR/T3
-  ln -s ./alice/data/type4-chosen_basis_indexes  ALICE/DIR/T4
-  ln -s ./alice/data/classical_channel_dest      ALICE/DIR/TCP_DEST
-  ln -s ./alice/data/classical_channel_src       ALICE/DIR/TCP_SRC
-  ln -s ./alice/logs                             ALICE/DIR/LOGS
-  ln -s ./alice/cmds                             ALICE/DIR/CMDS
+  ln -s "$DIR/alice/data/timestamp_events"            ALICE/DIR/TIMESTAMPS
+  ln -s "$DIR/alice/data/type1-time_and_click"        ALICE/DIR/T1
+  ln -s "$DIR/alice/data/type2-time_and_basis"        ALICE/DIR/T2
+  ln -s "$DIR/alice/data/type3-rawkey"                ALICE/DIR/T3
+  ln -s "$DIR/alice/data/type4-chosen_basis_indexes"  ALICE/DIR/T4
+  ln -s "$DIR/alice/data/classical_channel_dest"      ALICE/DIR/TCP_DEST
+  ln -s "$DIR/alice/data/classical_channel_src"       ALICE/DIR/TCP_SRC
+  ln -s "$DIR/alice/logs"                             ALICE/DIR/LOGS
+  ln -s "$DIR/alice/cmds"                             ALICE/DIR/CMDS
   # Pipes (Not declared here as the pipes are created by the program itself)
-  ln -s ./alice/pipes/                           ALICE/PIPES
+  ln -s "$DIR/alice/pipes"                       ALICE/PIPES
   mkfifo                                         ALICE/PIPES/chopper_to_transferd -m0600
   mkfifo                                         ALICE/PIPES/transferd_to_splicer -m0600
-  mkfifo                                         ALICE/PIPES/transferd_to_ec      -m0600
-  mkfifo                                         ALICE/PIPES/ec_to_transferd      -m0600
+  mkfifo                                         ALICE/PIPES/transferd_to_ec2      -m0600
+  mkfifo                                         ALICE/PIPES/ec_to_transferd2      -m0600
+
+  cd ../
+  DIR_CODE="$PWD"
+  echo $DIR_CODE
+  cd build
+  
   # Programs
-  ln -s ../errorcorrection/ecd2.o   ALICE/EXE/ECD2
-  ln -s ../errorcorrection/rnd.o    ALICE/EXE/RND
-  ln -s ../remotecrypto/chopper     ALICE/EXE/CHOPPER
-  ln -s ../remotecrypto/splicer     ALICE/EXE/SPLICER
-  ln -s ../remotecrypto/transferd   ALICE/EXE/TRANSFERD
+  ln -s "$DIR_CODE/errorcorrection/ecd2.o"   ALICE/EXE/ECD2
+  ln -s "$DIR_CODE/errorcorrection/rnd.o"    ALICE/EXE/RND
+  ln -s "$DIR_CODE/remotecrypto/chopper"     ALICE/EXE/CHOPPER
+  ln -s "$DIR_CODE/remotecrypto/splicer"     ALICE/EXE/SPLICER
+  ln -s "$DIR_CODE/remotecrypto/transferd"   ALICE/EXE/TRANSFERD
   # Timestamp files:
   # ALICE_TIMESTAMPS_DIR/aliceSample
 
   # Source code for readability
-  mkdir ALICE/SRC
-  ln -s ../errorcorrection/ecd2.c   ALICE/SRC/ECD2
-  ln -s ../errorcorrection/rnd.c    ALICE/SRC/RND
-  ln -s ../remotecrypto/chopper.c   ALICE/SRC/CHOPPER
-  ln -s ../remotecrypto/splicer.c   ALICE/SRC/SPLICER
-  ln -s ../remotecrypto/transferd.c ALICE/SRC/TRANSFERD
+  mkdir $DIR/ALICE/SRC
+  ln -s "$DIR_CODE/errorcorrection/ecd2.c"   ALICE/SRC/ECD2
+  ln -s "$DIR_CODE/errorcorrection/rnd.c"    ALICE/SRC/RND
+  ln -s "$DIR_CODE/remotecrypto/chopper.c"   ALICE/SRC/CHOPPER
+  ln -s "$DIR_CODE/remotecrypto/splicer.c"   ALICE/SRC/SPLICER
+  ln -s "$DIR_CODE/remotecrypto/transferd.c" ALICE/SRC/TRANSFERD
 
   cd ../scripts/setup
 else
--- scripts/setup/create_symbolic_links_bob.sh
+++ scripts/setup/create_symbolic_links_bob.sh
@@ -2,11 +2,15 @@
 
 # Create symbolic links for ease of access (See https://kb.iu.edu/d/abbe)
 # Ensure that the working directory is the setup directory.
+# Pre-requisites: run mkdir_bob.sh first.
 
 # Symbolic link container
 if [ ! -d ../../build/BOB ]
 then
   cd ../../build
+
+  DIR="$PWD"
+  echo $DIR
 
   mkdir BOB
   mkdir BOB/DIR
@@ -14,39 +18,45 @@ then
 
   # Bob
   # Directories
-  ln -s ./bob/data/timestamp_events              BOB/DIR/TIMESTAMPS
-  ln -s ./bob/data/type1-time_and_click          BOB/DIR/T1
-  ln -s ./bob/data/type2-time_and_basis          BOB/DIR/T2
-  ln -s ./bob/data/type3-rawkey                  BOB/DIR/T3
-  ln -s ./bob/data/type4-chosen_basis_indexes    BOB/DIR/T4
-  ln -s ./bob/data/classical_channel_dest        BOB/DIR/TCP_DEST
-  ln -s ./bob/data/classical_channel_src         BOB/DIR/TCP_SRC
-  ln -s ./bob/logs                               BOB/DIR/LOGS
-  ln -s ./bob/cmds                               BOB/DIR/CMDS
+  ln -s "$DIR/bob/data/timestamp_events"              BOB/DIR/TIMESTAMPS
+  ln -s "$DIR/bob/data/type1-time_and_click"          BOB/DIR/T1
+  ln -s "$DIR/bob/data/type2-time_and_basis"          BOB/DIR/T2
+  ln -s "$DIR/bob/data/type3-rawkey"                  BOB/DIR/T3
+  ln -s "$DIR/bob/data/type4-chosen_basis_indexes"    BOB/DIR/T4
+  ln -s "$DIR/bob/data/classical_channel_dest"        BOB/DIR/TCP_DEST
+  ln -s "$DIR/bob/data/classical_channel_src"         BOB/DIR/TCP_SRC
+  ln -s "$DIR/bob/logs"                               BOB/DIR/LOGS
+  ln -s "$DIR/bob/cmds"                               BOB/DIR/CMDS
   # Pipes
-  ln -s ./bob/pipes/                             BOB/PIPES
-  mkfifo                                         BOB/PIPES/transferd_to_pfindCostream -m0600
-  mkfifo                                         BOB/PIPES/costream_to_transferd      -m0600
-  mkfifo                                         BOB/PIPES/transferd_to_ec            -m0600
-  mkfifo                                         BOB/PIPES/ec_to_transferd            -m0600
+  ln -s "$DIR/bob/pipes"                              BOB/PIPES
+  mkfifo                                              BOB/PIPES/transferd_to_pfindCostream -m0600
+  mkfifo                                              BOB/PIPES/costream_to_transferd      -m0600
+  mkfifo                                              BOB/PIPES/transferd_to_ec            -m0600
+  mkfifo                                              BOB/PIPES/ec_to_transferd            -m0600
+
+  cd ../
+  DIR_CODE="$PWD"
+  echo $DIR_CODE
+  cd build
+
   # Programs
-  ln -s ../errorcorrection/ecd2.o     BOB/EXE/ECD2
-  ln -s ../errorcorrection/rnd.o      BOB/EXE/RND
-  ln -s ../remotecrypto/chopper2      BOB/EXE/CHOPPER2
-  ln -s ../remotecrypto/pfind         BOB/EXE/PFIND
-  ln -s ../remotecrypto/costream      BOB/EXE/COSTREAM
-  ln -s ../remotecrypto/transferd     BOB/EXE/TRANSFERD
+  ln -s "$DIR_CODE/errorcorrection/ecd2.o"     BOB/EXE/ECD2
+  ln -s "$DIR_CODE/errorcorrection/rnd.o"      BOB/EXE/RND
+  ln -s "$DIR_CODE/remotecrypto/chopper2"      BOB/EXE/CHOPPER2
+  ln -s "$DIR_CODE/remotecrypto/pfind"         BOB/EXE/PFIND
+  ln -s "$DIR_CODE/remotecrypto/costream"      BOB/EXE/COSTREAM
+  ln -s "$DIR_CODE/remotecrypto/transferd"     BOB/EXE/TRANSFERD
   # Timestamp files:
   # BOB_TIMESTAMPS_DIR/bobSample
 
   # Source code for readability
   mkdir BOB/SRC
-  ln -s ../errorcorrection/ecd2.c     BOB/SRC/ECD2
-  ln -s ../errorcorrection/rnd.c      BOB/SRC/RND
-  ln -s ../remotecrypto/chopper2.c    BOB/SRC/CHOPPER2
-  ln -s ../remotecrypto/pfind.c       BOB/SRC/PFIND
-  ln -s ../remotecrypto/costream.c    BOB/SRC/COSTREAM
-  ln -s ../remotecrypto/transferd.c   BOB/SRC/TRANSFERD
+  ln -s "$DIR_CODE/errorcorrection/ecd2.c"     BOB/SRC/ECD2
+  ln -s "$DIR_CODE/errorcorrection/rnd.c"      BOB/SRC/RND
+  ln -s "$DIR_CODE/remotecrypto/chopper2.c"    BOB/SRC/CHOPPER2
+  ln -s "$DIR_CODE/remotecrypto/pfind.c"       BOB/SRC/PFIND
+  ln -s "$DIR_CODE/remotecrypto/costream.c"    BOB/SRC/COSTREAM
+  ln -s "$DIR_CODE/remotecrypto/transferd.c"   BOB/SRC/TRANSFERD
 
   cd ../scripts/setup
 else
