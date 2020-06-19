8ca04c64cc4b0f8c815b24c4ec2194fb5f6bfd6f
Matt Lee <crazoter@gmail.com>
2020-06-19 19:38:08 +0800

    Update error correction log locations

--- scripts/execution/bob/init_ecd2.sh
+++ scripts/execution/bob/init_ecd2.sh
@@ -6,7 +6,7 @@ BOB/EXE/ECD2 \
   -r BOB/PIPES/trfrdToEc \
   -d BOB/DIR/T3_SIFTED \
   -f BOB/DIR/FINAL_KEY \
-  -l BOB/DIR/LOGS/ec_processed_epochs.txt \
-  -q BOB/DIR/LOGS/ec_responsepipe_placeholder.txt \
+  -l BOB/DIR/LOGS/ec/ec_processed_epochs.txt \
+  -q BOB/DIR/LOGS/ec/ec_responsepipe_placeholder.txt \
   -Q BOB/PIPES/ecQuery \
   > BOB/DIR/LOGS/ec/ec_stdout.txt
