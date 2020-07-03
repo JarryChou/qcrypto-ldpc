def7e63c79bf1d271edbd65e6cca134ee26fc5fb
matt lee <crazoter@gmail.com>
2020-07-03 12:56:17 +0800

    Update update_documentation.sh

--- update-documentation.sh
+++ update-documentation.sh
@@ -1,9 +1,11 @@
-# Checks out the gh-pages branch, merge and push
+# Helper script to update the documentation on the gh-pages branch as well
+
 git checkout gh-pages
 # Make sure to modify the branch name here if you use a different branch
 git rebase branch-ecd2-enhancements3
 git push gh-pages
 ./run_doxygen.sh
+git add .
 git commit -m "Update documentation"
 # You may need auth here
 git push
