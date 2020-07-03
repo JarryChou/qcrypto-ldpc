7b923d1a82983c6f0ae90e98cd763f0399d2101e
matt lee <crazoter@gmail.com>
2020-07-03 13:08:57 +0800

    Add check into run_doxygen.sh to remind users to update docs on gh-pages only

--- run_doxygen.sh
+++ run_doxygen.sh
@@ -1,5 +1,13 @@
 #!/bin/bash 
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$branch" != "gh-pages" ]; then
+	printf "Current branch is not gh-pages. Please checkout the gh-pages branch.\n"
+	printf "Alternatively, use ./update_documentation.sh.\n"
+	exit 0
+fi
+
 # This script runs doxygen to generate documentation to docs
 doxygen Doxyfile
 
