7f6fa98fa00265356dc13f4ed2b1fb7b088c6788
matt lee <crazoter@gmail.com>
2020-07-20 13:27:14 +0800

    Update build linux macos documentation

---
+++ errorcorrection/ldpc_examples/my_project_with_aff3ct/ci/build-linux-macos-compile-aff3ct.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# wrapper script to download & compile aff3ct together with project
+# if you have already compiled aff3ct, jsut call the script directly
+./build-linux-macos.sh COMPILE_AFF3CT="1"
--- errorcorrection/ldpc_examples/my_project_with_aff3ct/ci/build-linux-macos.sh
+++ errorcorrection/ldpc_examples/my_project_with_aff3ct/ci/build-linux-macos.sh
@@ -5,6 +5,9 @@
 
 set -x
 
+# See list of keys below.
+# Usage: ./build-linux-macos.sh VARIABLE="VALUE"
+# e.g. to compile AFF3CT: ./build-linux-macosh.sh COMPILE_AFF3CT=1
 # https://unix.stackexchange.com/questions/129391/passing-named-arguments-to-shell-scripts
 for ARGUMENT in "$@"
 do
@@ -14,28 +17,27 @@ do
 
     case "$KEY" in
             COMPILE_AFF3CT)     COMPILE_AFF3CT=${VALUE} ;;
+			AFF3CT_GIT_VERSION)	AFF3CT_GIT_VERSION=${VALUE} ;;
+			BUILD)				BUILD=${VALUE} ;;
             *)   
     esac    
 done
 
 if [ -z "$EXAMPLES" ]
 then
-	# Nobody is going to know what you are referring to...
-	# echo "Please define the 'EXAMPLES' environment variable."
-	# exit 1
-	# EXAMPLES=("bootstrap" "factory" "openmp" "tasks")
+	# Only one example in the current setup
 	EXAMPLES=("bootstrap")
 fi
 
 if [ -z "$AFF3CT_GIT_VERSION" ]
 then
-	echo "May want to define the 'AFF3CT_GIT_VERSION' environment variable. Defaulting to '2.3.5'."
+	echo "May want to define the 'AFF3CT_GIT_VERSION' variable. Defaulting to '2.3.5'."
 	AFF3CT_GIT_VERSION="2.3.5"
 fi
 
 if [ -z "$BUILD" ]
 then
-	echo "The 'BUILD' environment variable is not set, default value = 'build_linux_macos'."
+	echo "The 'BUILD' variable is not set, default value = 'build_linux_macos'."
 	BUILD="build_linux_macos"
 fi
 
@@ -55,8 +57,9 @@ then
 
 	mkdir $BUILD
 	cd $BUILD
-	# cmake .. -DCMAKE_BUILD_TYPE="Release" -DAFF3CT_COMPILE_EXE="OFF" -DAFF3CT_COMPILE_STATIC_LIB="ON" #-DCMAKE_CXX_FLAGS="-funroll-loops -march=native" -G"Unix Makefiles"
-	cmake .. -DCMAKE_BUILD_TYPE="Debug" -DAFF3CT_COMPILE_EXE="ON" -DAFF3CT_COMPILE_STATIC_LIB="ON" #-DCMAKE_CXX_FLAGS="-funroll-loops -march=native" -G"Unix Makefiles"
+	cmake .. -DCMAKE_BUILD_TYPE="Release" -DAFF3CT_COMPILE_EXE="OFF" -DAFF3CT_COMPILE_STATIC_LIB="ON" #-DCMAKE_CXX_FLAGS="-funroll-loops -march=native" -G"Unix Makefiles"
+	# Debug mode
+	# cmake .. -DCMAKE_BUILD_TYPE="Debug" -DAFF3CT_COMPILE_EXE="ON" -DAFF3CT_COMPILE_STATIC_LIB="ON" #-DCMAKE_CXX_FLAGS="-funroll-loops -march=native" -G"Unix Makefiles"
 	# Use 4 threads to compile
 	make -j4
 	cd ..
@@ -72,7 +75,9 @@ for example in ${EXAMPLES[*]}; do
 	cp ../../lib/aff3ct/${BUILD}/lib/cmake/aff3ct-$AFF3CT_GIT_VERSION/* cmake/Modules
 	mkdir $BUILD
 	cd $BUILD
-	cmake .. -DCMAKE_BUILD_TYPE="Debug" # -DCMAKE_CXX_FLAGS="-funroll-loops -march=native" -G"Unix Makefiles"
+	cmake .. -DCMAKE_CXX_FLAGS="-funroll-loops -march=native" -G"Unix Makefiles"
+	# Debug mode:
+	# cmake .. -DCMAKE_BUILD_TYPE="Debug" # -DCMAKE_CXX_FLAGS="-funroll-loops -march=native" -G"Unix Makefiles"
 	make -j $THREADS
 	cd ../..
 done
